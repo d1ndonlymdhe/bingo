485bef4df3ba5ffef6116d50e53308aaceb7f069
Ayush <ayushshrestha8920@protnmail.com>
2021-06-14 14:24:04 +0545

    client side basic structure complete

--- client/logic.js
+++ client/logic.js
@@ -3,6 +3,10 @@ var current = 1;
 var val;
 var replaceFrom="";
 var clickers= document.querySelectorAll("#clickers div");
+var input=[]
+for(let i = 0;i<25;i++){
+    input.push(0);
+}
 clickers.forEach(el=>{
     el.addEventListener('click',e=>{
         handle(el,e)
@@ -17,7 +21,11 @@ function handle(el,e){
         let text = document.createTextNode(current)
         p.appendChild(text)
         el.appendChild(p)
+        input[e.composedPath()[0].id]=current.toString()
         current++;
+        if(current==25){
+            showDone()
+        }
     }else{
         //console.log(el,replaceFrom,el==replaceFrom)
        if(replaceFrom==""){
@@ -40,10 +48,48 @@ function handle(el,e){
            replaceFrom.style.backgroundColor=""
            replaceFrom=""
            }
+           if(current == 25){
+               showDone()
+           }
            //console.log(replaceFrom)
            //console.log(replaceFrom.childNodes)
            //replaceFrom.appendChild(temp);
            
        }
     }
+}
+
+
+//handle random button
+var random= document.getElementById("randomize")
+random.addEventListener('click',e=>{
+    let arr=[]
+    for(let i=1;i<=25;i++){
+        arr.push(i)
+    }
+    arr = shuffleArray(arr);
+    //console.log(arr)
+    init(arr);
+    input = arr
+    showDone()
+})
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+function init(arr){
+    clickers.forEach((el,i)=>{
+        el.innerHTML=`<p>${arr[i]}</p>`;
+    })
+}
+
+function showDone(){
+    document.getElementById("done").classList.remove('hidden')
 }
